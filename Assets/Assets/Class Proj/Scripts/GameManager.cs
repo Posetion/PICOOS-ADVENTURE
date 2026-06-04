31f45991c04da4e1757bf50b0f08c5fb5df1c7bf
NOTYAMAL260 <thegreatreaper29@gmail.com>
2026-06-04 12:26:36 +0630

    timer

--- Assets/Assets/Class Proj/Scripts/GameManager.cs
+++ Assets/Assets/Class Proj/Scripts/GameManager.cs
@@ -1,25 +1,17 @@
 using UnityEngine;
-using UnityEngine.SocialPlatforms.Impl;
 
 public class GameManager : MonoBehaviour
 {
-
     public static GameManager instance;
 
     [Header("Game Configuration Settings")]
     [SerializeField] private int totalPlayerScore = 10;
     [SerializeField] private int totalTimeLimit = 60;
     [SerializeField] private int currentFruitScore = 0;
-    [SerializeField] private int currentTime = 0;
-    [SerializeField] private bool gameActive = false;
+    [SerializeField] private float currentTime = 0; // 1. Changed to float internally for smooth tracking
+    [SerializeField] private bool gameActive = true;
     [SerializeField] private bool canCompleteLevelFlag = false;
-
-    public int GetFruitScore()
-    {
-        return currentFruitScore;
-    }
-
-
+    [SerializeField] private bool timerIsRunning = true;
 
     private void Awake()
     {
@@ -31,25 +23,46 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-
     }
 
-    
+    private void Start()
+    {
+        // 2. Set the starting time to your time limit when the game begins
+        currentTime = totalTimeLimit;
+    }
 
+    private void Update()
+    {
+        if (!timerIsRunning) { return; }
 
+        // 3. Smoothly subtract float time elapsed
+        if (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+        }
+        else
+        {
+            currentTime = 0;
+            timerIsRunning = false;
+            gameActive = false;
+            Debug.Log("Time's Up!");
+        }
+    }
+
+    public int GetFruitScore()
+    {
+        return currentFruitScore;
+    }
+
+    // 4. Returns the float as a clean, rounded whole integer to your Timer script
+    public int GetTime()
+    {
+        return Mathf.CeilToInt(currentTime);
+    }
 
     public void Collect()
     {
-        // 1. Spawn the VFX if one is assigned
-        
         currentFruitScore += 1;
-
-        // 2. Add your game logic here (e.g., updating the inventory or score)
         Debug.Log($"Collected! You have {currentFruitScore} points.");
-
-        // TODO: ScoreManager.Instance.AddScore(scoreValue);
-
-        // 3. Destroy the collectible object
-       
     }
 }
---
+++ Assets/Assets/Class Proj/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    private TextMeshProUGUI text;
+
+    private void Awake()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Update()
+    {
+        int timeLeft = gameManager.GetTime();
+        text.text = $"Time Left : {timeLeft}s ";
+    }
+}
+
+
+
+
+
